f409b4a40ff829149d5cb2ca3870bc6659582ad5
Juan Valle <juanvalle@Juans-MacBook-Pro.local>
2023-01-24 23:42:41 -0600

    Bootstrap jQuery 1 and 2

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ $(function() {
             $("#carouselButton").children("i").removeClass("fa-pause");
             $("#carouselButton").children("i").addClass("fa-play");
         } else {
-            $(".carousel"),carousel("cycle");
+            $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
 });
